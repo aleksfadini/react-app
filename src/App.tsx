79a20ae52dcae8303ebd00d34551e6af9be16728
aleksfadini <aleksfadini@gmail.com>
2023-09-24 18:33:35 -0400

    about to delete this part and creating a button

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import ListGroup from "./components/ListGroup";
-let items = ["NY", "SF", "London", "Paris", "Tokyo"];
-
-const handleSelectItem = (item: string) => {
-  console.log(item);
-};
+import Alert from "./components/Alert";
 
 function App() {
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading={"Cities"}
-        onSelectedItem={handleSelectItem}
-      ></ListGroup>
+      <Alert>
+        Hello <span> hey</span>World
+      </Alert>
     </div>
   );
 }
